e9f484d0b64507919916857e27ec8e000d76028e
Aug-yu <55703018+Aug-yu@users.noreply.github.com>
2022-07-05 00:21:31 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://aug-yu.github.io/maotv/jk/bc.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
